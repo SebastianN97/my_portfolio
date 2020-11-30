ee2128a6c7595314149cafe5ea9458e2a02aab29
SebastianN97 <sebbe132132@gmail.com>
2020-11-30 19:50:12 +0100

    Styling contact page with semantic ui.

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,12 +3,41 @@ import { UndrawAddUser } from "react-undraw-illustrations";
 
 const Contact = () => {
   return (
-    <div className="ui main container">
+    <div id="contact-main" className="ui main container">
       <div class="column">
-      <img src="https://www.benify.com/app/uploads/contact_top_image.jpg" alt="contact" width="800" height="400"/>
+        <img
+          id="contact-img"
+          src="https://www.benify.com/app/uploads/contact_top_image.jpg"
+          alt="contact"
+          width="800"
+          height="400"
+        />
+      </div>
+
+      <div>
+        <h1>Contact Information</h1>
       </div>
 
       <div id="contact-box" class="ui stackable two column grid">
+        <div class="column">
+          <div class="ui list">
+            <div class="item">
+              <i class="user icon"></i>
+              <div class="content">Sebastian Niewiadomski</div>
+            </div>
+            <div class="item">
+              <i class="phone icon"></i>
+              <div class="content">076 911 5199</div>
+            </div>
+            <div class="item">
+              <i class="mail icon"></i>
+              <div class="content">
+                <a href="mailto:sebbe132132@gmail.com">sebbe132132@gmail.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div class="column">
           <div class="ui list">
             <div class="item">
@@ -17,50 +46,6 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <div class="column">
-          <div class="ui list">
-            <div class="item">
-              <i class="mail icon"></i>
-              <div class="content">
-                <a href="mailto:sebbe132132@gmail.com">sebbe132132@gmail.com</a>
-              </div>
-            </div>
-
-
-
-            <div class="item">
-              <i class="linkify icon"></i>
-              <div class="content">
-                <a href="https://www.linkedin.com/in/sebastian-niewiadomski-8a65421b7/">
-                  linkedin.com/SebastianNiewiadomski
-                </a>
-              </div>
-            </div>
-            <div class="item">
-              <i class="linkify icon"></i>
-              <div class="content">
-                <a href="">github.com/SebastianN97</a>
-              </div>
-            </div>
-
-
-
-          </div>
-        </div>
-
-        <div class="column">
-        <div class="item">
-              <i class="linkify icon"></i>
-              <div class="content">
-                <a href="https://www.linkedin.com/in/sebastian-niewiadomski-8a65421b7/">
-                  linkedin.com/SebastianNiewiadomski
-                </a>
-              </div>
-            </div>
-        </div>
-
-
-
       </div>
     </div>
   );
